6b976084c1feca66fb0f6ba7a714927576130f8d
Arthur63 <Arthur63@163.com>
2024-03-17 18:36:24 +0800

    tweak error macro

--- util.h
+++ util.h
@@ -1,11 +1,8 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-/*
- * 遇到致命错误并退出
- */
-
-#define error(FMT, ...) eprintf ("%s: " FMT "\n", __FUNCTION__, ##__VA_ARGS__)
+#define error(FMT, ...)                                                       \
+  eprintf ("%s (%d): " FMT "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 extern void eprintf (const char *fmt, ...)
     __attribute__ ((noreturn, format (printf, 1, 2)));
